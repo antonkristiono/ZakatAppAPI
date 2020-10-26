67dffed7e8fd4b246e33cd6308578b6aff94086b
Anton Kristiono <antonkristiono47@gmail.com>
2020-10-26 18:28:35 +0700

    aa

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const dotenv = require ('dotenv');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+app.get('/',(req,res) => {
+    res.send('Hello ini adalah Express')
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
