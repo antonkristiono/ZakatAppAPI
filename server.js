37d4c134df71be80a01cfc0c0b2a366a09e76782
Anton Kristiono <antonkristiono47@gmail.com>
2020-12-07 18:58:53 +0700

    aaa

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const logger = require('./middleware/logger')
+const connectDB = require ('./config/db');
+const colors = require ('colors');
+// load env variable
+dotenv.config({ path: './config/config.env' });
+
+// connect DB
+connectDB();
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
 
-// load env variable
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -17,7 +22,16 @@ app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
-    console.log(` Server Berjalan di ${process.env.NODE_ENV} mode di port ${PORT}`)
+    console.log(` Server Berjalan di ${process.env.NODE_ENV} mode di port ${PORT}`.yellow.bold)
 );
+
+
+// Handle unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red.bold);
+    // Close Server & Exit process
+    server.close(() => process.exit(1));
+})
